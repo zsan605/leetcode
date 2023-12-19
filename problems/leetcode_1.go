fcebf2eef47fc20e6451cce09f1d4b6be2c866a5
Zhang San <zsan605@gmail.com>
2023-12-19 12:54:34 +0800

    Update leetcode_1.go
    新增方法

--- problems/leetcode_1.go
+++ problems/leetcode_1.go
@@ -14,6 +14,27 @@ func twoSum(nums []int, target int) []int {
 
 // 以空间换时间
 func twoSum2(nums []int, target int) []int {
+
+	if len(nums) < 2 {
+		return nil
+	}
+
+	var numsMap = make(map[int]int)
+	for i, val := range nums {
+		numsMap[target-val] = i
+	}
+
+	for i, val := range nums {
+		j, ok := numsMap[val]
+		if ok && i != j {
+			return []int{i, j}
+		}
+	}
+	return nil
+}
+
+// 以空间换时间，优化版
+func twoSum3(nums []int, target int) []int {
 	tempMap := make(map[int]int)
 
 	for i, val := range nums {
